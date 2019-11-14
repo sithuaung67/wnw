438e5cd4bc6b3d8d9a15d6000407d8490aa0b02d
SoeMoeThu <wpa28developer@gmail.com>
2019-11-14 13:39:15 +0630

    first

--- resources/views/admin/layouts/_partial/meta.blade.php
+++ resources/views/admin/layouts/_partial/meta.blade.php
@@ -1,6 +1,8 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="csrf-token" content="{{ csrf_token() }}">
-<title>AdminLTE 3 | Dashboard</title>
+<title>Monitoring System</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" type="image/png" href="{{asset('favicon.ico')}}">
+
 
